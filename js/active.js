db6fcd8e2078d6ced11334f663b7e7c2346fa19b
Mrrobotravi <ravikumawat9898@gmail.com>
2024-04-08 03:07:49 +0530

    new update in home page

--- js/active.js
+++ js/active.js
@@ -193,3 +193,16 @@
     }
 
 })(jQuery);
+
+
+$(document).ready(function() {
+    setTimeout(function() {
+      $('#container').addClass('loaded');
+      // Once the container has finished, the scroll appears
+      if ($('#container').hasClass('loaded')) {
+        // It is so that once the container is gone, the entire preloader section is deleted
+        $('#preloader').delay(9000).queue(function() {
+          $(this).remove();
+        });}
+    }, 3000);});
+  
